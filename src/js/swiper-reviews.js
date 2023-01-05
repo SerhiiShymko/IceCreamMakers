999e948fc878b2d1f4174ac2ed70fefad17b4c15
SerhiiShymko <shimko456@gmail.com>
2023-01-05 16:30:45 +0200

    swiper

--- src/js/swiper-reviews.js
+++ src/js/swiper-reviews.js
@@ -1,26 +1,25 @@
-$(document).ready(function(){
-
-  $(".slider").slick({
-     arrows: false,
-     dots: true,
-     speed: 500,
-     easing: "ease",
-     infinite: false,
-     autoplay: true,
-     autoplaySpeed: 2500,
-     pauseOnFocus: true,
-     pauseOnHover: true,
-     pauseOnDotsHover: true,
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    speed: 500,
+    easing: 'ease',
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
   });
 
-  $(".top-slider").slick({
-     arrows: false,
-     speed: 500,
-     easing: "ease",
-     infinite: true,
-     autoplay: true,
-     autoplaySpeed: 3000,
-     pauseOnFocus: true,
-     pauseOnHover: true,
+  $('.top-slider').slick({
+    arrows: false,
+    speed: 500,
+    easing: 'ease',
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
   });
-})
+});
