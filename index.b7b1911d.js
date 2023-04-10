982566f3a6869125f86ed8cb9e9c91ecafc4d604
SerhiiShymko <113916099+SerhiiShymko@users.noreply.github.com>
2023-04-10 13:30:07 +0000

    Deploying to gh-pages from @ SerhiiShymko/IceCreamMakers@4fd8498ad2ec33b28ada0fd4a2a59cbc2124aad5 🚀

---
+++ index.b7b1911d.js
@@ -0,0 +1,2 @@
+$(".slider").slick({arrows:!1,dots:!0,speed:500,easing:"ease",infinite:!1,autoplay:!0,autoplaySpeed:2500,pauseOnFocus:!0,pauseOnHover:!0,pauseOnDotsHover:!0}),$(".top-slider").slick({arrows:!1,speed:500,easing:"ease",infinite:!0,autoplay:!0,autoplaySpeed:3e3,pauseOnFocus:!0,pauseOnHover:!0});
+//# sourceMappingURL=index.b7b1911d.js.map
--- index.f23193cf.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,speed:500,easing:"ease",infinite:!1,autoplay:!0,autoplaySpeed:2500,pauseOnFocus:!0,pauseOnHover:!0,pauseOnDotsHover:!0}),$(".top-slider").slick({arrows:!1,speed:500,easing:"ease",infinite:!0,autoplay:!0,autoplaySpeed:3e3,pauseOnFocus:!0,pauseOnHover:!0})}));
-//# sourceMappingURL=index.f23193cf.js.map
