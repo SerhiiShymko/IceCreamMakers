8b5aecb0d0f4661d2270cf2f07394a07e217564f
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-07-08 22:34:02 +0300

    feature/modal-product2

--- src/js/modal-product.js
+++ src/js/modal-product.js
@@ -1,14 +1,46 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal__open--product-cream]'),
-    closeModalBtn: document.querySelector('[data-modal__close--product-cream]'),
-    modal: document.querySelector('[data-modal__product-cream]'),
+    openIceCream: document.querySelector('[data-modal__open--product-cream]'),
+    closeIceCream: document.querySelector('[data-modal__close--product-cream]'),
+    iceCreamModal: document.querySelector('[data-modal__product-cream]'),
+
+
+    openIceCoffee: document.querySelector(
+      '[data-modal__open--product-icecoffee]'
+    ),
+    closeIceCoffee: document.querySelector(
+      '[data-modal__close--product-icecoffee]'
+    ),
+    iceCoffeeModal: document.querySelector('[data-modal__product-icecoffee]'),
+
+
+    openMilkShakes: document.querySelector(
+      '[data-modal__open--product-milkshakes]'
+    ),
+    closeMilkShakes: document.querySelector(
+      '[data-modal__close--product-milkshakes]'
+    ),
+    milkShakesModal: document.querySelector('[data-modal__product-milkshakes]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openIceCream.addEventListener('click', toggleIceCream);
+  refs.closeIceCream.addEventListener('click', toggleIceCream);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  refs.openIceCoffee.addEventListener('click', toggleIceCoffee);
+  refs.closeIceCoffee.addEventListener('click', toggleIceCoffee);
+
+  refs.openMilkShakes.addEventListener('click', toggleMilkShakes);
+  refs.closeMilkShakes.addEventListener('click', toggleMilkShakes);
+
+  function toggleIceCream() {
+    refs.iceCreamModal.classList.toggle('is-hidden');
+  }
+
+  function toggleIceCoffee() {
+    refs.iceCoffeeModal.classList.toggle('is-hidden');
+  }
+
+  function toggleMilkShakes() {
+    refs.milkShakesModal.classList.toggle('is-hidden');
   }
 })();
